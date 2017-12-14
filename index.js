a6d0c4aa8ac694c91fdcd3ccbd0490ef553c6f8e
Rob Dowers <rdowers@gmail.com>
2017-12-14 15:23:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ function nestedTarget() {
 // - Define a function `increaseRankBy(n)` that increases the ranks in all of the `.ranked-list`s by `n`. (You might need to make use of [`parseInt()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt)
 
 function increaseRankBy(n) {
-  
+  var lis = document.querySelectorAll("ul.ranked-list");
+  for (let i=0; i < lis.length; i++) {
+    
+  }
 }
 
 // - Define a function `deepestChild()` that pulls out the most deeply nested child from `div#grand-node`. (Remember, you can iterate over elements and call `querySelector()` and `querySelectorAll()` on them. This is challenging to implement correctly, but not beyond your ability!)
