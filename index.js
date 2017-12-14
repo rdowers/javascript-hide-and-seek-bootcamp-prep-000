cb4f084a443370d1dd6247e4a0c1c1e62371d2f8
Rob Dowers <rdowers@gmail.com>
2017-12-14 15:17:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function getFirstSelector(selector) {
 // - Define a function `nestedTarget()` that pulls a `.target` out of `#nested` (`#` is used for IDs in selectors — but you knew that because you [read the docs](https://developer.mozilla.org/en-US/docs/Web/Guide/CSS/Getting_Started/Selectors), right? :) ). (Note that in `index.html` `#nested` and `.target` just _happen_ to be `div`s. This method should work with arbitrary elements.)
 
 function nestedTarget() {
-  return document.querySelector("#nested.target");
+  return document.querySelector("#nested .target");
 }
 
 // - Define a function `increaseRankBy(n)` that increases the ranks in all of the `.ranked-list`s by `n`. (You might need to make use of [`parseInt()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt)
