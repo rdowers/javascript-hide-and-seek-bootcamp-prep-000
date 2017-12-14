9e75f2aaef06748b6dfd77d89b1fe85d37e0d54d
Rob Dowers <rdowers@gmail.com>
2017-12-14 15:09:10 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+// - Define a function `getFirstSelector(selector)`, which accepts a selector and returns the first element that matches.
+
+
+
+// - Define a function `nestedTarget()` that pulls a `.target` out of `#nested` (`#` is used for IDs in selectors — but you knew that because you [read the docs](https://developer.mozilla.org/en-US/docs/Web/Guide/CSS/Getting_Started/Selectors), right? :) ). (Note that in `index.html` `#nested` and `.target` just _happen_ to be `div`s. This method should work with arbitrary elements.)
+
+
+
+// - Define a function `increaseRankBy(n)` that increases the ranks in all of the `.ranked-list`s by `n`. (You might need to make use of [`parseInt()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt)
+
+
+
+// - Define a function `deepestChild()` that pulls out the most deeply nested child from `div#grand-node`. (Remember, you can iterate over elements and call `querySelector()` and `querySelectorAll()` on them. This is challenging to implement correctly, but not beyond your ability!)
+
+
