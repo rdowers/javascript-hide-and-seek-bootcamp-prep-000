045d5ccfc39591c698cf1f2f174eeb60734e2dce
Rob Dowers <rdowers@gmail.com>
2017-12-14 15:28:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function nestedTarget() {
 function increaseRankBy(n) {
   var lis = document.querySelectorAll("ul.ranked-list li");
   for (let i=0; i < lis.length; i++) {
-    lis[i].innerHTML = parseInt(lis[i]) + n;
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
   }
 }
 
