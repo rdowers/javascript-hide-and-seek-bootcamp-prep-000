a7738869b7b0812001f39b97bf3fcf42624f4eb5
Rob Dowers <rdowers@gmail.com>
2017-12-14 15:29:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function nestedTarget() {
 }
 
 // - Define a function `increaseRankBy(n)` that increases the ranks in all of the `.ranked-list`s by `n`. (You might need to make use of [`parseInt()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt)
+//Done
 
 function increaseRankBy(n) {
   var lis = document.querySelectorAll("ul.ranked-list li");
